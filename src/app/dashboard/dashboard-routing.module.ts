d567b34cb0e6fd523ef1afd91afc753bfc48dce4
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-07 08:52:44 +0300

    edit errored imports.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { DashboardPage } from './dashboard.page';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
+
 
 const routes: Routes = [
   {
@@ -11,7 +15,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forChild(routes), ],
+  exports: [RouterModule], providers: 
+  [Geolocation,NativeGeocoder]
+
 })
 export class DashboardPageRoutingModule {}
