8f28d17504d62609b0a1b02f4a045ce4d4246417
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-07 08:55:29 +0300

    edit map imports

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { IonBottomDrawerModule } from 'ion-bottom-drawer';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
+import 'hammerjs';
 
 const routes: Routes = [
   {
@@ -21,6 +23,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    IonBottomDrawerModule,
     DashboardPageRoutingModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule
