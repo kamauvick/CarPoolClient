d8c277e2a8c427c56b8b954ec6b27203a0b040dd
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-07 16:03:19 +0300

    add routing for request ride and offer ride tabs.

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -9,14 +9,25 @@ import { NativeGeocoder } from '@ionic-native/native-geocoder/ngx';
 
 const routes: Routes = [
   {
-    path: '',
-    component: DashboardPage
+    path: "",
+    component: DashboardPage,
+    children:[
+      {
+      path:"offer-ride",
+      loadChildren: "../offer-ride/offer-ride.module#OfferRidePageModule"
+      },
+      {
+        path: "request-ride",
+        loadChildren:"../request-ride/request-ride.module#RequestRidePageModule"
+      }
+    ]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes), ],
-  exports: [RouterModule], providers: 
+  exports: [RouterModule],
+  providers: 
   [Geolocation,NativeGeocoder]
 
 })
