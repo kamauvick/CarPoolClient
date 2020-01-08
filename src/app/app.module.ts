abb93ec61e8b401236256a8a9f0e3f2435620902
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-08 19:36:09 +0300

    add calender plugin.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 // Work around Hammerjs
 import 'hammerjs';
 
