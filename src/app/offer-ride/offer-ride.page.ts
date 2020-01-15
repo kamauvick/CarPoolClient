5ac266bfec40ef2322a49c23be816195e9fe7012
washucode <estherw8525@gmail.com>
2020-01-15 12:39:50 +0300

    create offer class with Destination and Origin  inported

--- src/app/offer-ride/offer-ride.page.ts
+++ src/app/offer-ride/offer-ride.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { DashboardPage } from '../dashboard/dashboard.page';
+import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+let options: NativeGeocoderOptions = {
+  useLocale: true,
+  maxResults: 5
+};
 @Component({
   selector: 'app-offer-ride',
   templateUrl: './offer-ride.page.html',
@@ -9,12 +14,21 @@ import { DashboardPage } from '../dashboard/dashboard.page';
 export class OfferRidePage implements OnInit {
 
   userInput = {'destination':'Limuru','source':'Nairobi'}
-  constructor(public router : Router) { }
+  constructor(public router : Router,private nativeGeocoder: NativeGeocoder) { }
       
   ngOnInit() {
+    this.convertAddressToLatLong()
   }
- goToDashboard=()=>{
+  convertAddressToLatLong=()=>{
+    this.nativeGeocoder.forwardGeocode(this.userInput.destination, options)
+  .then((result: NativeGeocoderResult[]) => console.log('The coordinates are latitude=' + result[0].latitude + ' and longitude=' + result[0].longitude))
+  .catch((error: any) => console.log(error));
+  }
   
+ goToDashboard=()=>{
    this.router.navigate(['/dashboard'],{queryParams:{destination:this.userInput.destination,source:this.userInput.source}})
+  
  }
+
+ 
 }
