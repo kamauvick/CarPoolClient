5dbf7c454dd12ed01afb715b3921ac8f77d25260
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-21 17:25:41 +0300

    add code for lat tong conversion.

--- src/app/offer-ride/offer-ride.page.ts
+++ src/app/offer-ride/offer-ride.page.ts
@@ -1,12 +1,9 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
-import { NativeGeocoder, NativeGeocoderResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
 
-let options: NativeGeocoderOptions = {
-  useLocale: true,
-  maxResults: 5
-};
 
+declare var google: any;
+ 
 @Component({
   selector: 'app-offer-ride',
   templateUrl: './offer-ride.page.html',
@@ -14,23 +11,55 @@ let options: NativeGeocoderOptions = {
 })
 
 export class OfferRidePage implements OnInit {
+  
+  
+  sourceLat: number = 0;
+  sourceLong: number = 0;
+  destinationLat: number = 0;
+  destinationLong: number = 0;
+  userInput = { "destination": "", "source": "" }
+  sourceCoords = {}
+  destinationCoords = {}
+  
+  
 
+  constructor(private router: Router) {
+    
+  }
 
-  userInput = {'destination':'Limuru','source':'Nairobi'}
-  constructor(public router : Router,private nativeGeocoder: NativeGeocoder) { }
-      
+  
   ngOnInit() {
-    this.convertAddressToLatLong()
+    //this.convertAddressToLatLong()
+    
   }
   convertAddressToLatLong=()=>{
-    this.nativeGeocoder.forwardGeocode(this.userInput.destination, options)
-  .then((result: NativeGeocoderResult[]) => console.log('The coordinates are latitude=' + result[0].latitude + ' and longitude=' + result[0].longitude))
-  .catch((error: any) => console.log(error));
-
+ 
   }
   
- goToDashboard=()=>{
-   this.router.navigate(['/dashboard'],{queryParams:{destination:this.userInput.destination,source:this.userInput.source}})
+    //convert Address string to lat and long
+    geoCode(address:any) {
+      let geocoder = new google.maps.Geocoder();
+      console.log('found source'+ this.userInput.source)
+      //let source = this.userInput.source
+      geocoder.geocode({'address':this.userInput.source,}, (results, status) => {
+      this.sourceLat = results[0].geometry.location.lat();
+      this.sourceLong = results[0].geometry.location.lng();
+      let sourceCoords = {latitude:this.sourceLat,longitude:this.sourceLong}
+      console.log('Source coordinates are: '+this.sourceLat,this.sourceLong)
+    console.log("source lat: " +sourceCoords.latitude + ",source long: " + sourceCoords.longitude);
+     });
+     geocoder.geocode({'address':this.userInput.destination}, (results, status) => {
+      this.destinationLat = results[0].geometry.location.lat();
+      this.destinationLong = results[0].geometry.location.lng();
+      let destinationCoords = {latitude:this.destinationLat,longitude:this.destinationLong}
+      console.log('Destination coordinates are: '+this.destinationLat,this.destinationLong)
+      console.log("destination lat: " + destinationCoords.latitude + ", destination long: " + destinationCoords.longitude);
+     });
+     this.goToDashboard();
+     
+   }
   
+ goToDashboard=()=>{
+  this.router.navigate(['/dashboard'],{queryParams:{destination:this.userInput.destination,source:this.userInput.source}})  
  }
 }
