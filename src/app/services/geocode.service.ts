8efb567410741659b7bbf615a89c076616d2f65d
Christine-N-Mwaura <njerimwaura25@gmail.com>
2020-01-21 17:26:19 +0300

    add a service for autocomplete.

--- src/app/services/geocode.service.ts
+++ src/app/services/geocode.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare var google: any;
 
@@ -8,9 +9,16 @@ declare var google: any;
 })
 
 export class GeocodeService {
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
 
+  getResults(search_term,latitude,longitude){
+    const app_id='OdOjehas2P4I0fqX4K9A'
+    const app_code='krlpdHD5HfkLst-2yrtt8A'
+    const url=`https://places.cit.api.here.com/places/v1/autosuggest?at=${latitude},${longitude}&q=${search_term}&app_id=${app_id}&app_code=${app_code}`
+    return this.http.get(url)
+    // console.log('in interval')
+  }
 }
 
 
